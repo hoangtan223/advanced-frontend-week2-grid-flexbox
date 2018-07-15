4592689fd3464e9974bbf87afd0fe438996b2204
Tan Bui <hoangtan223@gmail.com>
2018-07-16 02:47:42 +0800

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Weather/>
+  </div>
+</template>
+
+<script>
+import Weather from './components/Weather'
+import './assets/main.scss'
+
+export default {
+  name: 'App',
+  components: {
+    Weather
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'PT Sans', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffffff;
+  height: 100vh;
+}
+</style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class='container'>
+    <div class="column">
+      <div class="time">
+        <span>7:30 am</span>
+        <span>Tue 23rd Feb</span>
+      </div>
+      <div></div>
+      <div class="location">
+        <span class="country">- Sweden</span>
+        <span class="city">Stockholm</span>
+      </div>
+    </div>
+    <div class="column">
+      <img src="../assets/Weather.svg" alt="weather logo">
+    </div>
+    <div class="column">
+      <span class="degree">-7</span>
+      <div class="description">
+        <span>Clear</span>
+        <span>with periodic</span>
+        <span>cloud</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Weather',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
